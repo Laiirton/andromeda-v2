b0674722aa65d4f0958c5db39f2a1be050fd050c
Lairton <130435095+Laiirton@users.noreply.github.com>
2026-03-03 16:32:31 -0300

    Update client.js

--- src/core/client.js
+++ src/core/client.js
@@ -1,10 +1,20 @@
 'use strict';
 
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const { Client, RemoteAuth } = require('whatsapp-web.js');
 const config = require('../config');
+const SupabaseStore = require('../store/SupabaseStore');
+
+const store = new SupabaseStore({
+    supabaseUrl: config.supabase.url,
+    supabaseKey: config.supabase.key,
+});
 
 const client = new Client({
-    authStrategy: new LocalAuth(),
+    authStrategy: new RemoteAuth({
+        clientId: 'andromeda', // Define nome do arquivo no bucket: RemoteAuth-andromeda.zip
+        store,
+        backupSyncIntervalMs: 300_000, // Salva backup a cada 5 minutos
+    }),
     puppeteer: config.puppeteer,
 });
 
